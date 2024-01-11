c724c51667562cfd19fbace4fbb445e2e2b79cc1
Victor Fernandes <90485435+VictorFMag@users.noreply.github.com>
2024-01-11 15:15:25 -0300

    Add files via upload

--- backend/server.js
+++ backend/server.js
@@ -5,43 +5,53 @@ app.use(cors());
 app.use(express.json());
 const port = 3000;
 
+const bcrypt = require('bcrypt'); // Encripta a senha
+const saltRounds = 10;
+
 const userController = require('./controller/userController');
 const db = require('./database/database')
 db.sync(() => console.log(`Banco de dados conectado: ${process.env.DB_NAME}`));
 
+// Fazer login
 app.post('/api/login', (req, res) => {
     const { username, password } = req.body;
 
     userController.getUser(username)
         .then(userData => {
-            if (userData.password === password) {
+            const hashCorreto = bcrypt.compareSync(password, userData.password); // true or false
+
+            if (hashCorreto == true) {
                 userData.isAdmin ? res.status(200).send({ success: true, isAdmin: true }) : res.status(200).send({ success: true, isAdmin: false });
             } else {
-                res.status(500).send({ success: false, message: "Credenciais incorretas!" });
-            };
+                res.status(500).send({ success: false, message: "Senha incorreta" })
+            }
         })
         .catch(error => {
             res.status(500).send({ message: 'Erro interno ao buscar usuário' });
         });
 });
 
+// Cadastro de usuarios
 app.post('/cadastro', (req, res) => {
-    userController.createUser({
-        userName: req.body.userName,
-        password: req.body.password,
-        isAdmin: req.body.isAdmin
-    })
-        .then((item) => res.send(item))
-        .catch((err) => {
-            console.log('Erro no cadastro do usuario', JSON.stringify(err))
-            return res.status(400).send(err)
-        });
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+        userController.createUser({
+            userName: req.body.userName,
+            password: hash,
+            isAdmin: req.body.isAdmin
+        })
+            .then((item) => res.send(item))
+            .catch((err) => {
+                console.log('Erro no cadastro do usuario', JSON.stringify(err))
+                return res.status(400).send(err)
+            });
+    });
 });
 
+//Listar todos os usuarios
 app.get('/cadastro', (req, res, next) => {
     userController.listUsers().then((items) => res.send(items))
         .catch((err) => {
-            console.log('Erro na consulta do usuário', JSON.stringify(err))
+            console.log('Erro na consulta dos usuários', JSON.stringify(err))
             return res.send(err)
         });
 });
